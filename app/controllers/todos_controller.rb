e3183e41a123de81fc76777250b299384bbde0a5
py563 <maredukondaprajwal@gmail.com>
2021-10-14 20:20:57 +0530

    implemented create todo and resources convention

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,8 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  # for postman to work
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
@@ -8,5 +11,17 @@ class TodosController < ApplicationController
     id = params[:id]
     todo = Todo.find(id)
     render plain: todo.to_pleasant_string
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    response_text = "Hey, your new todo is create with id #{new_todo.id}"
+    render plain: response_text
   end
 end
