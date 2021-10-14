384074956bdfd53b9f9f8c222bb5843101a6a356
py563 <maredukondaprajwal@gmail.com>
2021-10-14 19:27:04 +0530

    can see single todo and list in a prettier way

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+  def to_pleasant_string
+    display_status = completed ? "[X]" : "[ ]"
+    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{display_status}"
+  end
 end
