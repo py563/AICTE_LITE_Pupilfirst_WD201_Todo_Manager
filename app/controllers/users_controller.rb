70397bf1532a454416dc674c88a397a189205122
py563 <maredukondaprajwal@gmail.com>
2021-10-18 17:14:02 +0530

    added create new user and simple login module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # users_controller.rb
 class UsersController < ApplicationController
   # for postman to work
-  # skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def index
     response_text = "Hey, below are list of users:"
@@ -15,7 +15,28 @@ class UsersController < ApplicationController
   end
 
   def create
-    response_text = "Hey, your new todo is create with id"
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+
+    response_text = "Hey, #{name} we have created a user for you with email: #{email}"
+    render plain: response_text
+  end
+
+  def login
+    email = params[:email]
+    password = params[:password]
+    user = User.find_by(email: email, password: password)
+    response_text = "Wrong Email or Password"
+    if user.present?
+      response_text = "Hey Welcome #{user.to_pleasant_string}"
+    end
     render plain: response_text
   end
 end
