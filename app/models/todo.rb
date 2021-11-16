16cd639cea65c597d74f0859dee16fce4178c5b6
py563 <maredukondaprajwal@gmail.com>
2021-11-16 14:00:49 +0530

    implemented checked, striking, rem overdue compltd

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,15 +4,15 @@ class Todo < ActiveRecord::Base
   end
 
   def self.due_today
-    all.where("due_date = ?", Date.today)
+    where("due_date = ?", Date.today)
   end
 
   def self.due_later
-    all.where("due_date > ?", Date.today)
+    where("due_date > ?", Date.today).order(:due_date)
   end
 
   def self.overdue
-    all.where("due_date < ?", Date.today)
+    where("due_date < ? and not completed", Date.today).order(:due_date)
   end
 
   def self.completed
