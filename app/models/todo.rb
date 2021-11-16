dbda39fb1af622dc50e823661dbf007e88d6fdc4
py563 <maredukondaprajwal@gmail.com>
2021-11-16 12:09:29 +0530

    bugfix db migrations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,39 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    display_status = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{display_status}"
+  def is_due_today?
+    due_date == Date.today
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+    puts "Overdue \n"
+    overdue?.map { |todo_item| puts todo_item.to_displayable_string }
+
+    puts "\n\n"
+    puts "Due Today\n"
+    due_today?.map { |todo_item| puts todo_item.to_displayable_string }
+
+    puts "\n\n"
+    puts "Due Later\n"
+    due_later?.map { |todo_item| puts todo_item.to_displayable_string }
+  end
+
+  def self.mark_as_complete(completed_item_id)
+    Todo.update(completed_item_id, completed: true)
   end
 end
