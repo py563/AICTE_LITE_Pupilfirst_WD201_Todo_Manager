146f7fec1824a4a28ad4fc6ae3e78210f5ded195
py563 <maredukondaprajwal@gmail.com>
2021-11-26 12:58:08 +0530

    added migration for users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_052006) do
+ActiveRecord::Schema.define(version: 2021_11_26_071419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -45,9 +45,12 @@ ActiveRecord::Schema.define(version: 2021_11_16_052006) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
-    t.text "email"
-    t.text "password"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
